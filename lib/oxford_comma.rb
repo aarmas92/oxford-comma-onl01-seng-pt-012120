8b55336a60dede7e58c2e1e6fc0881cac97e58f7
Alvaro Armas <aarmas92@outlook.com>
2020-01-30 16:33:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma array
-    if array.length==1 #this line check array length if it prints that element
+    if array.length==1
         puts array[0]
-    else #this block executes if array length greater than 1
-        last=array[array.length-1] #this line used to store last element of the array
-        array.pop #this line delete the last element of the array
-        string=array.join(', ')#this line join the all the elements of the array to the string except last element
-        if array.length==1#this line check to array length after delete last element
-            string.concat(" and ")#this line adds two elements of the array
+    else 
+        last=array[array.length-1] 
+        array.pop 
+        string=array.join(', ')
+        if array.length==1
+            string.concat(" and ")
         else
-            string.concat(", and ")#this line add multiple elements of the array to the string
+            string.concat(", and ")
         end
         string.concat(last)#this line adds the last element of the array to the string
         puts string #this line prints the string from the array
